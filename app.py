868be42d66eb12cc43e6d58d1b128dcfdc43c2d7
Praise2112 <oketola.praise@gmail.com>
2020-01-29 15:46:21 +0700

    Added classifySpam route

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+import pandas as pd
+import pickle
+# heroku git:remote -a {your-project-name}
 
 import pickle
 
@@ -22,6 +27,53 @@ def hello_world():
 
 @app.route('/classifyNews', methods=['GET', 'POST'])
 def classifyNews():
+
+    data = request.json
+    text = data['text']
+
+    result = dict()
+    result['prediction'] = classifier.predict([text])[0]
+
+    return result
+
+
+@app.route('/classifySpam', methods=['GET', 'POST'])
+def classifySpam():
+
+    df = pd.read_csv('spam2.csv', encoding='latin-1')
+    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1,
+            inplace=True)
+    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    X = df['message']
+    y = df['label']
+
+    # Extract feature with CountVectorize
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.33,
+                                                        random_state=42)
+    from sklearn.naive_bayes import MultinomialNB
+
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+
+    data = request.json
+    text = data['text']
+    vect = cv.transform([text]).toarray()
+    pred = clf.predict(vect)
+    print(pred)
+
+    result = dict()
+    result['prediction'] = str(pred[0])
+
+    return result
+
+
+@app.route('/sentiment', methods=['GET', 'POST'])
+def sentiment():
 
     data = request.json
     text = data['text']
